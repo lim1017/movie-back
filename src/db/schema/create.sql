b350398b31ae415364065b6be49d200426627466
tom <lim1017@hotmail.com>
2020-09-01 00:41:01 -0400

    can save nominations

--- src/db/schema/create.sql
+++ src/db/schema/create.sql
@@ -5,5 +5,5 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) UNIQUE,
-  nominations JSON
+  nominations json
 );
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,21 +2,22 @@ const router = require("express").Router();
 
 module.exports = db => {
   
-  router.get("/users", (request, response) => {
+  // router.get("/users", (request, response) => {
 
-    db.query(
-      `
-      SELECT *
-      FROM users
-    `,
-    ).then(({ rows: results }) => {
-      response.json(results);
-    });
-  });
+  //   db.query(
+  //     `
+  //     SELECT *
+  //     FROM users
+  //   `,
+  //   ).then(({ rows: results }) => {
+  //     response.json(results);
+  //   });
+  // });
   
   router.put("/users/register", (request, response) => {
-
+    
     const { username } = request.body
+    console.log(username)
     let nominations = []
 
     db.query(
@@ -29,27 +30,61 @@ module.exports = db => {
     `, [username, nominations]
     ).then(({ rows: results }) => {
       response.json(results);
-    });
+    })
+    .catch( err =>{
+
+      response.status(400);
+      response.send('None shall pass');
+
+      // console.log(err)
+      // response.send(err)
+    })
+  });
+
+
+  router.get("/users/:username", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
+
+    const username=request.params.username
+
+    db.query(
+      `
+      SELECT * FROM users
+      WHERE username = $1
+      `,
+      [username]
+    )
+      .then(({ rows: user }) => {
+        response.json(user);
+      })
+      .catch(error => console.log(error));
   });
 
 
   router.patch("/users/:userID", (request, response) => {
 
     const userID = request.params.userID
-
     const {nominations} = request.body
+
+    console.log(userID)
+    console.log(nominations)
 
     db.query(
       `
       UPDATE users
       SET
-      nominations = $1,
+      nominations = $1
   
       WHERE user_id= $2
     `, [nominations, userID]
     ).then(({ rows: results }) => {
       response.json(results);
-    });
+    }).catch( err =>{
+      console.log(err)
+    })
   });
   
 
